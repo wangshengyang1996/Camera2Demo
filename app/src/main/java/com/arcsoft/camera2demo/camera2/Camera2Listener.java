9dc0890f59e708e3b4380e33ae4c818dc64ae581
wsy9057 <wsy9057@arcsoft.com.cn>
2019-07-05 17:41:08 +0800

    用于演示Camera2的使用

---
+++ app/src/main/java/com/arcsoft/camera2demo/camera2/Camera2Listener.java
@@ -0,0 +1,37 @@
+package com.arcsoft.camera2demo.camera2;
+
+
+import android.hardware.camera2.CameraDevice;
+import android.util.Size;
+
+public interface Camera2Listener {
+    /**
+     * 当打开时执行
+     * @param cameraDevice 相机实例
+     * @param cameraId 相机ID
+     * @param displayOrientation 相机预览旋转角度
+     * @param isMirror 是否镜像显示
+     */
+    void onCameraOpened(CameraDevice cameraDevice, String cameraId, Size previewSize, int displayOrientation, boolean isMirror);
+
+    /**
+     * 预览数据回调
+     * @param y 预览数据，Y分量
+     * @param u 预览数据，U分量
+     * @param v 预览数据，V分量
+     * @param previewSize  预览尺寸
+     */
+    void onPreview(byte[] y, byte[] u, byte[] v, Size previewSize);
+
+    /**
+     * 当相机关闭时执行
+     */
+    void onCameraClosed();
+
+    /**
+     * 当出现异常时执行
+     * @param e 相机相关异常
+     */
+    void onCameraError(Exception e);
+
+}
---
+++ app/src/main/java/com/arcsoft/camera2demo/util/ImageUtil.java
@@ -0,0 +1,24 @@
+package com.arcsoft.camera2demo.util;
+
+public class ImageUtil {
+    /**
+     * 将Y:U:V == 4:2:2的数据转换为nv21
+     *
+     * @param y    Y 数据
+     * @param u    U 数据
+     * @param v    V 数据
+     * @param nv21 生成的nv21，需要预先分配内存
+     */
+    public static void yuv422ToYuv420sp(byte[] y, byte[] u, byte[] v, byte[] nv21) {
+        System.arraycopy(y, 0, nv21, 0, y.length);
+        int nv21UVIndex = y.length;
+        int length = y.length + u.length / 2 + v.length / 2;
+        int uIndex = 0, vIndex = 0;
+        for (int i = nv21UVIndex; i < length; i += 2) {
+            vIndex += 2;
+            uIndex += 2;
+            nv21[i] = v[vIndex];
+            nv21[i + 1] = u[uIndex];
+        }
+    }
+}
